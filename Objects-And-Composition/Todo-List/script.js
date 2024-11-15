501fd4b8fd69f736873524f7ce1a864d0daad35c
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-11-15 17:56:03 +0200

    Change variable types to const

--- Objects-And-Composition/Todo-List/script.js
+++ Objects-And-Composition/Todo-List/script.js
@@ -3,9 +3,9 @@ document.getElementById("todo-list").addEventListener("click", deleteTask);
 function addTask() {
 	const input = document.getElementById("todo-input");
 
-	let taskElement = document.createElement("li");
-	let taskDeleteBtn = document.createElement("button");
-	let todoList = document.getElementById("todo-list");
+	const taskElement = document.createElement("li");
+	const taskDeleteBtn = document.createElement("button");
+	const todoList = document.getElementById("todo-list");
 
 	const taskTextContent = input.value.trim();
 	if (taskTextContent.length == 0) {
