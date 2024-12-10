eded05b0547634fe65992cb0dadf3aba4f021e42
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2024-12-10 15:50:21 +0200

    Implement create element handler functionality

--- Dom-Introduction/task13/script.js
+++ Dom-Introduction/task13/script.js
@@ -3,11 +3,22 @@ const inputTextElement = document.querySelector("input");
 const buttonCreateElement = document.querySelector("button");
 
 buttonCreateElement.addEventListener("click", handleCreateElement);
-itemsListElement.addEventListener("click", handleEditElementText)
+itemsListElement.addEventListener("click", handleEditElementText);
 function handleCreateElement() {
-	console.log('create element')
+	const inputValue = inputTextElement.value;
+
+	if (inputValue.length == 0) {
+		alert("Please enter text into input field!");
+		return;
+	}
+
+	const listItemElement = document.createElement("li");
+	listItemElement.textContent = inputValue;
+	itemsListElement.appendChild(listItemElement);
+
+	inputTextElement.value = "";
 }
 
 function handleEditElementText(event) {
-	console.log('edit text')
+	console.log("edit text");
 }
