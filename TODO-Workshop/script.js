41aa8b90264d77c326457f80568c30f8ad858d66
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-01-11 15:07:29 +0200

    Create delete button and attack it to the created task

--- TODO-Workshop/script.js
+++ TODO-Workshop/script.js
@@ -2,6 +2,12 @@ const addTaskBtn = document.getElementById("add-btn");
 const input = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 
+const createDeleteBtn = () => {
+	const deleteBtn = document.createElement("button");
+	deleteBtn.textContent = "X";
+	deleteBtn.className = "delete-btn";
+	return deleteBtn;
+};
 const createTask = () => {
 	const inputValue = input.value;
 	if (inputValue.trim().length == 0) {
@@ -13,8 +19,11 @@ const createTask = () => {
 	task.className = "todo-item";
 	task.textContent = inputValue;
 
+	const deleteBtn = createDeleteBtn();
+	task.appendChild(deleteBtn);
 	return task;
 };
+
 const handleAddTask = () => {
 	const newTask = createTask();
 	if (!newTask) {
@@ -23,4 +32,7 @@ const handleAddTask = () => {
 	todoList.appendChild(newTask);
 };
 
+const handleDeleteTask = () => {
+	//
+};
 addTaskBtn.addEventListener("click", handleAddTask);
