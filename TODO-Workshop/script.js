4761ca546b6b6e215bf8e97672b3b50dad0cd611
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-01-12 13:31:43 +0200

    Add comments

--- TODO-Workshop/script.js
+++ TODO-Workshop/script.js
@@ -3,6 +3,7 @@ const input = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 const resetInputBtn = document.getElementById("reset-btn");
 
+// If there is currentId in localStorage it points to the last created task id
 const getCurrentId = () => {
 	let currentId = localStorage.getItem("currentId");
 	if (!currentId) {
@@ -12,6 +13,7 @@ const getCurrentId = () => {
 };
 let currentId = getCurrentId();
 
+// If there are tasks saved in localStorage return them as array, if NOT set empty array as taskList in localStorage and return it.
 const getTasksList = () => {
 	let tasksList = JSON.parse(localStorage.getItem("tasksList"));
 	if (!tasksList) {
